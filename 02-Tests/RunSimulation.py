fc840d70a242b704d4a4814afa37b327d990d5db
Kenneth Long <k.long@imperial.ac.uk>
2021-02-24 10:29:27 +0000

    First simulation test

---
+++ 02-Tests/RunSimulation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test script for "Simulation" class ... initialisation and get methods
+==================================
+
+  Simulation.py -- set "relative" path to code
+
+"""
+
+import Simulation as Simu
+
+##! Start:
+print("========  Simulation: start  ========")
+print()
+
+Smltn = Simu.Simulation(5000, 5., '11-Parameters/nuSTORM-PrdStrght-Params-v1.0.csv')
+
+print()
+print(".... Execute simulation")
+
+Smltn.RunSim()
+
+##! Complete:
+print()
+print("========  Simulation: complete  ========")
