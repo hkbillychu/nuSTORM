d4d4573b3d66606722dfef4212333cb99010e148
Ken Long <k.long@imperial.ac.uk>
2021-05-20 18:27:08 +0100

    Corrected startup.bash logic

--- startup.bash
+++ startup.bash
@@ -2,16 +2,17 @@
 
 stm=$(PWD)
 #echo $stm
-add="/01-Code"
-#echo $add
-dir="$stm$add"
-#echo $dir
 
+dir="$stm"
 echo "Set nuSIM path:"
 nuSIMPATH="$dir"
 echo "    " $nuSIMPATH
 export nuSIMPATH
 
+add="/01-Code"
+#echo $add
+dir="$stm$add"
+#echo $dir
 echo "Set PYTHON path:"
 PYTHONPATH="${PYTHONPATH}:$dir"
 echo "    " $PYTHONPATH
