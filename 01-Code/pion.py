e2e6c81896574a1be6fec187f9a0dfcfa56bbb21
Paul Kyberd <Paul.Kyberd@gmail.com>
2021-08-27 09:35:06 +0100

    pion and pionTst ... problems with pionTst because of mismatch with traceSpace version

---
+++ 01-Code/pion.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Class pion:
+===========
+
+  Description of a pion
+
+  Class attributes:
+
+  --np       : numpy class
+      
+  Instance attributes:
+  --------------------
+  _TrcSpcCrd: Trace space (s, x, y, z, x', y') in numpy array - what are s and z?
+  _t        : time in nanoseconds
+  _p        : Pion 4 momentum: (E, array(px, py, pz)), GeV
+  _weight   : not sure how to define
+    
+  Methods:
+  --------
+  Built-in methods __new__, __repr__ and __str__.
+      __init__ : Creates decay instance
+      __repr__ : One liner with call.
+      __str__  : Dump of values of decay
+
+  Get/set methods:
+  
+  General methods:
+
+Created on Tue 20Aug12;35: Version history:
+----------------------------------------------
+ 1.0: 20Aug21: First implementation
+ 1.1: 22Aug21: Use the traceSpace class
+
+@author: PaulKyberd
+"""
+
+import math
+import numpy as np
+from copy import deepcopy
+import PionConst as PionConst
+import traceSpace
+
+piCnst = PionConst.PionConst()
+    
+class pion:
+
+    __pimass = piCnst.mass()/1000.
+    __Debug  = False
+    
+#--------  "Built-in methods":
+    def __init__(self, s, x, y, z, px, py, pz, t):
+
+        E = math.sqrt(px*px + py*py + pz*pz + self.__pimass*self.__pimass)
+        p = np.array([px, py, pz])
+        self._p = np.array([E, np.array([px, py, pz])],dtype=object)
+        self._TrcSpc = traceSpace.traceSpace(s, x, y, z, px/pz, py/pz)
+        self._t = t
+        self._weight = 0
+        return
+
+    def __repr__(self):
+        return "pion(x, y, z, s, px, py, pz, t)"
+
+    def __str__(self):
+        return "pion: E (GeV) = %g, p (GeV) = (%g  %g  %g),  t = %g, s = %g, x = %g, y = %g, z = %g, x' = %g , y' = %g " % \
+                  (self._p[0], self._p[1][0],  self._p[1][1], self._p[1][2],self._t, self._TrcSpc.s(), self._TrcSpc.x(), 
+                    self._TrcSpc.y(), self._TrcSpc.z(), self._TrcSpc.xp() ,self._TrcSpc.yp()  )
+    
+#--------  get/set methods:
+    def getP(self):
+        return deepcopy(self._p)
+
+    def traceSpace(self):
+        return deepcopy(self._TrcSpc)
+
+    def t(self):
+        return deepcopy(self._t)
+
+#    def getcostheta(self):
+#        return deepcopy(self._costheta)
+
+#    def getTraceSpaceCoord(self):
+#        return deepcopy(self._TrcSpcCrd)
+
+#    def getmu4mmtm(self):
+#        return deepcopy(self._P_mu)
+
+#    def getnumu4mmtm(self):
+#        return deepcopy(self._P_numu)
+
+#    def getppiGen(self):
+#        return deepcopy(self._ppiGen)
+if __name__ == "__main__":
+    print("in main(pion)")
+    x = pion(0.1, 0.15, -100.0, 0.0, 0.2, 0.22, 5.12, 0.00)
+
+    print(x)
+
