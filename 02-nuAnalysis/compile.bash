f6a8366fd8081c7cf2d4b430c90c9792f0e13ca7
Kenneth Long <k.long@imperial.ac.uk>
2021-11-25 15:40:03 +0000

    Quick tidy

--- 02-nuAnalysis/compile.bash
+++ 02-nuAnalysis/compile.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ 01-Code/RunControl.cpp 02-Tests/RunControlTst.cpp -I01-Code `root-config --cflags --libs` -o 12-Bin/RunControlTst.cpp
+g++ 01-Code/RunControl.cpp 01-Code/nuAnalysis.cpp 03-Skeleton/nuAnalysis-skeleton.cpp -I01-Code `root-config --cflags --libs` -o 12-Bin/nuAnalysis-skeleton.exe
