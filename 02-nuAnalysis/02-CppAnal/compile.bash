1442976ec33162d1bd39dbc57335660efa40d12b
pfaffm <84314640+pfaffm@users.noreply.github.com>
2021-11-29 16:36:39 +0000

    Added checking for directory before running compile command in bash scripts (#17)

--- 02-nuAnalysis/02-CppAnal/compile.bash
+++ 02-nuAnalysis/02-CppAnal/compile.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+stm=$PWD
+add="/12-Bin"
+dir="$stm$add"
+
+if [[ ! -d $dir ]]
+then
+  echo "$dir does exists on your filesystem."
+  echo "Making directory..."
+  mkdir $dir
+fi
+
 g++ 01-Code/RunControl.cpp 01-Code/nuAnalysis.cpp 03-Skeleton/nuAnalysis-skeleton.cpp -I01-Code `root-config --cflags --libs` -o 12-Bin/nuAnalysis-skeleton.exe
