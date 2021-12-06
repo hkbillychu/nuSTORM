ba64a3f98d24a8e16026c36caf04c82970918be1
Ken Long <k.long@imperial.ac.uk>
2021-12-06 10:40:02 +0000

    Rudimentary documentatiomn

--- 02-nuAnalysis/02-CppAnal/03-Skeleton/nuAnalysis-skeleton-compile.bash
+++ 02-nuAnalysis/02-CppAnal/03-Skeleton/nuAnalysis-skeleton-compile.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ 01-Code/RunControl.cpp 01-Code/nuAnalysis.cpp 03-Skeleton/nuAnalysis-skeleton.cpp -I01-Code `root-config --cflags --libs` -o 12-Bin/nuAnalysis-skeleton.exe
+g++ $nuAnalysisCPATH/RunControl.cpp $nuAnalysisCPATH/nuAnalysis.cpp ./nuAnalysis-skeleton.cpp -I$nuAnalysisCPATH `root-config --cflags --libs` -o nuAnalysis-skeleton.exe
