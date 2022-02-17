Normalisation bug fix (#24)
* Added timestructure in normalisation code plus some other small fixes

* Added a class to access nuSTORM design constants in order to avoid circular import problems.

* Added functionality to input target particle into PionEventInstance to propagate proper dynamical properties through whole setup. Minor updates to PionConst and RandomGenerator.

* Adapted normalisation to include non-pencil like beam according to bdSIM distribution. Needed to include eventHistory.cd() method in order to fix ROOT Writing problem.

* Introduced changes to avoid reading the ROOT file every time a RandomGenerator method is called and load the histograms into memory instead.

* Fixed wrong transformation from nuSTORM global to transfer line local coordinates in PionEventInstance and added changes in calling of RandomGenerator constructor and methods to normalisation.py .

Co-authored-by: kyberdp <78826893+kyberdp@users.noreply.github.com>

01-nuSIM/04-Studies/normalisation.py
@@ -553,7 +553,7 @@ if __name__ == "__main__" :
 
 
 # Get machine and run parameters
-    RndmGen = Rndm.RandomGenerator(rootInputFilename)
+    RndmGen = Rndm.RandomGenerator(rootInputFilename,'histP','histXPS')
     nuStrt = nuPrdStrt.nuSTORMPrdStrght(filename)
     psLength = nuStrt.ProdStrghtLen()
     nuTrLnCmplx = nuTrf.nuSTORMTrfLineCmplx(trfCmplxFile)
@@ -573,10 +573,10 @@ if __name__ == "__main__" :
 for event in range(nEvents):
 # set its values
     s = 0.0
-    xl, xpl = RndmGen.getRandom2D('histXPS')
-    yl, ypl = RndmGen.getRandom2D('histYPS')
+    xl, xpl = RndmGen.getRandom2D()
+    yl, ypl = RndmGen.getRandom2D()
     zl = -tlCmplxLength
-    pPion = RndmGen.getRandom('histP')
+    pPion = RndmGen.getRandom()
     pxl = pPion*xpl
     pyl = pPion*ypl
     pzl = np.sqrt(pPion*pPion - pxl**2 - pyl**2)
@@ -617,7 +617,6 @@ for event in range(nEvents):
         normInst.decayPiInPS()
 # decay the muons
         if (muDcyFlag): normInst.decayMuons()
-
 
 
 #  write to the root structure